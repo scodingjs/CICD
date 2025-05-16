c831260532aece1c38ccfc9badaad38f29c7ce87
Sanganu <kppkmsanu@gmail.com>
2025-05-16 12:39:59 -0600

    adding

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,11 +11,5 @@ export default defineConfig({
     },
   },
 
-  e2e: {
-    baseUrl: 'http://localhost:3001',
-   
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+
 });
