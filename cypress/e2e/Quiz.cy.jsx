0fc1bcad02165d53c98c066a95abbd2d6744ba47
Sanganu <kppkmsanu@gmail.com>
2025-05-16 20:36:56 -0600

    Port 3001 in yml and config

--- cypress/e2e/Quiz.cy.jsx
+++ cypress/e2e/Quiz.cy.jsx
@@ -1,6 +1,6 @@
 describe('Quiz App - Render Deployment', () => {
   it('should load the quiz page ', () => {
-    cy.visit('https://cicd-1suy.onrender.com/');
+    cy.visit('/');
     cy.get('button').contains('Start Quiz').click();
     // cy.get('[data-cy=question-text]')
     //   .should('exist')
