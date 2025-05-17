86d5dcda0777318964654513eab6b038767f0569
Sanganu <kppkmsanu@gmail.com>
2025-05-16 18:31:51 -0600

    yml/cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     },
   },
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
   }
